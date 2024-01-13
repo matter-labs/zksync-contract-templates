7ed1628b35a46e808b726a47a1a3b474a6f93ac1
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2024-01-14 01:43:58 +0200

    feat: add ethers v6 version (#4)

--- templates/hardhat_ethers5/solidity/test/erc20/myerc20token.test.ts
+++ templates/hardhat_ethers5/solidity/test/erc20/myerc20token.test.ts
@@ -17,28 +17,26 @@ describe("MyERC20Token", function () {
 
   it("Should have correct initial supply", async function () {
     const initialSupply = await tokenContract.totalSupply();
-    expect(initialSupply).to.equal(BigInt("1000000000000000000000000")); // 1 million tokens with 18 decimals
+    expect(initialSupply.toString()).to.equal("1000000000000000000000000"); // 1 million tokens with 18 decimals
   });
 
   it("Should allow owner to burn tokens", async function () {
-    const burnAmount = ethers.parseEther("10"); // Burn 10 tokens
-    const tx = await tokenContract.burn(burnAmount);
-    await tx.wait();
+    const burnAmount = ethers.utils.parseEther("10"); // Burn 10 tokens
+    await tokenContract.burn(burnAmount);
     const afterBurnSupply = await tokenContract.totalSupply();
-    expect(afterBurnSupply).to.equal(BigInt("999990000000000000000000")); // 999,990 tokens remaining
+    expect(afterBurnSupply.toString()).to.equal("999990000000000000000000"); // 999,990 tokens remaining
   });
 
   it("Should allow user to transfer tokens", async function () {
-    const transferAmount = ethers.parseEther("50"); // Transfer 50 tokens
-    const tx = await tokenContract.transfer(userWallet.address, transferAmount);
-    await tx.wait();
+    const transferAmount = ethers.utils.parseEther("50"); // Transfer 50 tokens
+    await tokenContract.transfer(userWallet.address, transferAmount);
     const userBalance = await tokenContract.balanceOf(userWallet.address);
-    expect(userBalance).to.equal(transferAmount);
+    expect(userBalance.toString()).to.equal(transferAmount.toString());
   });
 
   it("Should fail when user tries to burn more tokens than they have", async function () {
-    const userTokenContract = new Contract(await tokenContract.getAddress(), tokenContract.interface, userWallet);
-    const burnAmount = ethers.parseEther("100"); // Try to burn 100 tokens
+    const userTokenContract = new Contract(tokenContract.address, tokenContract.interface, userWallet);
+    const burnAmount = ethers.utils.parseEther("100"); // Try to burn 100 tokens
     try {
       await userTokenContract.burn(burnAmount);
       expect.fail("Expected burn to revert, but it didn't");
