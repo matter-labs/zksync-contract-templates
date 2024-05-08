2109ceb2b2b2efe4b0cc0bca945844d37f711a2a
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-05-08 15:38:02 -0500

    feat: add foundry factory templates

---
+++ templates/quickstart/foundry/factory/script/DeployFactory.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/CrowdfundFactory.sol";
+import "../src/CrowdfundingCampaign.sol";
+
+contract DeployFactoryAndCreateCampaign is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("WALLET_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the CrowdfundingFactory contract
+        CrowdfundingFactory factory = new CrowdfundingFactory();
+
+        // Log the factory's address
+        console.log("CrowdfundingFactory deployed at: %s", address(factory));
+
+        // Define the funding goal for the new campaign
+        uint256 fundingGoalInWei = 0.01 ether;
+
+        // Use the factory to create a new CrowdfundingCampaign
+        factory.createCampaign(fundingGoalInWei);
+
+        // Not sure how to get the address of the new campaign
+        // TODO: Log the address of the new campaign
+
+        vm.stopBroadcast();
+    }
+}
