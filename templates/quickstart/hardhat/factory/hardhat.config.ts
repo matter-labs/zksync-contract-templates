c78a801a832c2a030d73eec0ef955f7e85acab70
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-05-07 13:26:54 -0600

    chore: update factory template

--- templates/quickstart/hardhat/factory/hardhat.config.ts
+++ templates/quickstart/hardhat/factory/hardhat.config.ts
@@ -1,43 +1,48 @@
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/config";
 
 import "@matterlabs/hardhat-zksync";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  defaultNetwork: "zkSyncSepoliaTestnet",
+  defaultNetwork: "ZKsyncEraSepolia",
   networks: {
-    zkSyncSepoliaTestnet: {
+    ZKsyncEraSepolia: {
       url: "https://sepolia.era.zksync.dev",
       ethNetwork: "sepolia",
       zksync: true,
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
+      verifyURL:
+        "https://explorer.sepolia.era.zksync.dev/contract_verification",
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [process.env.WALLET_PRIVATE_KEY]
+        : [],
     },
-    zkSyncMainnet: {
+    ZKsyncEraMainnet: {
       url: "https://mainnet.era.zksync.io",
       ethNetwork: "mainnet",
       zksync: true,
-      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-    },
-    sophonTestnet: {
-      url: "https://rpc.testnet.sophon.xyz",
-      ethNetwork: "sepolia",
-      verifyURL: "https://api-explorer-verify.testnet.sophon.xyz/contract_verification",
-      zksync: true,
-    },
-    sophonMainnet: {
-      url: "https://rpc.sophon.xyz",
-      ethNetwork: "mainnet",
-      verifyURL: "https://verification-explorer.sophon.xyz/contract_verification",
-      zksync: true,
+      verifyURL:
+        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [process.env.WALLET_PRIVATE_KEY]
+        : [],
     },
     dockerizedNode: {
       url: "http://localhost:3050",
       ethNetwork: "http://localhost:8545",
       zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [process.env.WALLET_PRIVATE_KEY]
+        : [],
     },
     anvilZKsync: {
       url: "http://127.0.0.1:8011",
       ethNetwork: 'http://localhost:8545',
       zksync: true,
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [process.env.WALLET_PRIVATE_KEY]
+        : [],
     },
     hardhat: {
       zksync: true,
@@ -46,12 +51,13 @@ const config: HardhatUserConfig = {
   zksolc: {
     version: "latest",
     settings: {
+      codegen: 'yul',
       // find all available options in the official documentation
       // https://docs.zksync.io/build/tooling/hardhat/hardhat-zksync-solc#configuration
     },
   },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.24",
   },
 };
 
